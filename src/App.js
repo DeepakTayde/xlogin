48eaba32a88664d6e4410395b507a48261f05243
DeepakTayde <deepaktayde559@gmail.com>
2025-06-28 23:29:26 +0530

    login component implemented

--- src/App.js
+++ src/App.js
@@ -2,40 +2,57 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const [isLogin, setIsLogin] =useState(true)
+  const [isLogin, setIsLogin] = useState(true);
+  const [error, setError] = useState("");
 
-
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(username === 'user' && password==='password'){
-      setIsLogin(false)
-    }else{
-      alert('userName and Password is incorrect')
+    if (username === "user" && password === "password") {
+      setIsLogin(false);
+      setError("");
+    } else {
+      setError("Invalid username or password");
     }
-  }
+  };
   return (
     <div className="App">
       <div className="loginContainer">
         <h1>Login Page</h1>
+        {!isLogin && <h5>Welcome, user!</h5>}
+        {error && ( <div style={{marginTop:'10px', marginBottom:'10px'}}>{error}</div>)}
         {isLogin && (
-                  <form onSubmit={(e)=>handleSubmit(e)}>
-          <div>
-            <label for="username">UserName:</label>
-            <input type="text" name="username" value={username} placeholder="username" onChange={(e) => setUsername(e.target.value)} required />
-          </div>
-          <div>
-            <label for="password">Password:</label>
-            <input type="text" name="password" value={password} placeholder="password" onChange={(e) => setPassword(e.target.value)} required />
-          </div>
-          <button type="submit">submit</button>
-        </form>
-        )}
+          <form onSubmit={handleSubmit} className="formContainer">
+            <label htmlFor="username">
+              Username:
+              <input
+                id="username"
+                type="text"
+                name="username"
+                value={username}
+                placeholder="username"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
 
-        {!isLogin &&(
-          <h5>welcome to login page...</h5>
+            <label htmlFor="password">
+              Password:
+              <input
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+
+            <button type="submit">submit</button>
+          </form>
         )}
       </div>
     </div>
