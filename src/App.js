2bc672faa73cfe421ccf8d5cf390ef3129d2d339
DeepakTayde <deepaktayde559@gmail.com>
2025-06-28 20:25:50 +0530

    implemented login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isLogin, setIsLogin] =useState(true)
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(e.target.value === 'user' && e.target.value==='password'){
+      setIsLogin(false)
+    }
+  }
   return (
     <div className="App">
-      <h1>Login Page</h1>
+      <div className="loginContainer">
+        <h1>Login Page</h1>
+        {isLogin && (
+                  <form onSubmit={(e)=>handleSubmit(e)}>
+          <div>
+            <label for="username">UserName:</label>
+            <input type="text" name="username" value="" alt="username" required />
+          </div>
+          <div>
+            <label for="password">Password:</label>
+            <input type="text" name="password" value="" alt="password" required />
+          </div>
+          <button type="submit">submit</button>
+        </form>
+        )}
+
+        {!isLogin &&(
+          <h5>welcome to login page...</h5>
+        )}
+      </div>
     </div>
   );
 }
