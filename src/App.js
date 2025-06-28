6f58c8b6602e227c76aaaa9deba778f10ed0a02b
DeepakTayde <deepaktayde559@gmail.com>
2025-06-28 20:29:43 +0530

    implemented login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+    const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const [isLogin, setIsLogin] =useState(true)
 
 
   const handleSubmit=(e)=>{
     e.preventDefault();
-    if(e.target.value === 'user' && e.target.value==='password'){
+    if(username === 'user' && password==='password'){
       setIsLogin(false)
+    }else{
+      alert('userName and Password is incorrect')
     }
   }
   return (
@@ -19,11 +24,11 @@ function App() {
                   <form onSubmit={(e)=>handleSubmit(e)}>
           <div>
             <label for="username">UserName:</label>
-            <input type="text" name="username" value="" alt="username" required />
+            <input type="text" name="username" value={username} placeholder="username" onChange={(e) => setUsername(e.target.value)} required />
           </div>
           <div>
             <label for="password">Password:</label>
-            <input type="text" name="password" value="" alt="password" required />
+            <input type="text" name="password" value={password} placeholder="password" onChange={(e) => setPassword(e.target.value)} required />
           </div>
           <button type="submit">submit</button>
         </form>
